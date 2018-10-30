91b73c00c591342e23abdc0c34f1d7f23f5dd712
AGilberg <Adrian.B.Gilberg@Gmail.com>
2018-10-30 11:45:33 +0100

    oppdatert filer

--- test2d.js
+++ test2d.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var groundLevel = canvas.height - 100;
 var posX = canvas.width / 2;
-var posY = groundLevel;
+var posY;
+var groundLevel = canvas.height - 100;
 var dx = 0;
 var dy = 0;
 var upReleased = false;
@@ -16,9 +16,15 @@ var audioCounter = 0;
 var counter = 0;
 var menuCounter = 0;
 var game = false;
+var menu = true;
+var credits = false;
+var instructions = false;
 var friction = 0.9;
 var doubleJump = false;
 var lastRight = true;
+var death = false;
+var srcX = 0;
+var srcXShuriken = 0;
 
 var audio = new Audio();
 audio.src = "sakura.mp3";
@@ -29,10 +35,10 @@ var idle = new Image();
 idle.src = "img/idle.png";
 var idleL = new Image();
 idleL.src = "img/idleL.png";
-var runleft = new Image();
-runleft.src = "img/runleft.png";
-var runright = new Image();
-runright.src = "img/runright.png";
+var runLeft = new Image();
+runLeft.src = "img/runLeft.png";
+var runRight = new Image();
+runRight.src = "img/runRight.png";
 var jumpMidL = new Image();
 jumpMidL.src = "img/jumpMidL.png";
 var jumpMidR = new Image();
@@ -45,9 +51,13 @@ var imageSound = new Image();
 imageSound.src = muteSound.src;
 var menuBackground = new Image();
 menuBackground.src = "img/Ninja.jpg";
+var imageBack = new Image();
+imageBack.src = "img/back.jpg";
+var deathAnimation = new Image();
+deathAnimation.src = "img/dead.png";
 
-//konstruerer objekter med info om sprites som brukes til animasjon
-class spriteAnimationConstructor {
+//Class with objects containing information of sprites, and functions to draw them
+class SpriteAnimationConstructor {
   constructor(
     spriteWidth,
     spriteHeight,
@@ -63,6 +73,66 @@ class spriteAnimationConstructor {
     this.frameCount = frameCount;
     this.source = source;
   }
+
+  //animate main character at posx and posy with information depending on sprite used.
+  animateMainChar() {
+    ctx.drawImage(
+      this.source,
+      this.updateIndex(),
+      0,
+      this.spriteWidth / this.frameCount,
+      this.spriteHeight,
+      posX,
+      posY,
+      this.charWidth,
+      this.charHeight
+    );
+  }
+  //returns which sprite from mainChar spritesheet to use
+  updateIndex() {
+    if (counter == 3 && death == false) {
+      curFrame = (curFrame + 1) % this.frameCount;
+      counter = 0;
+    } else if (counter == 10 && death == true) {
+      curFrame = (curFrame + 1) % this.frameCount;
+      counter = 0;
+    } else {
+      if (death == false || curFrame <= 8) {
+        counter++;
+      }
+    }
+
+    //Calculating the x coordinate for spritesheet
+    return (curFrame * this.spriteWidth) / this.frameCount;
+  }
+
+  //Draw shuriken if throwing == true
+  drawShuriken() {
+    if (throwing == true) {
+      posShuriken += velocityShuriken;
+      ctx.drawImage(
+        shuriken,
+        this.updateFrameShuriken(),
+        0,
+        shurikenSprite.spriteWidth / shurikenSprite.frameCount,
+        shurikenSprite.spriteHeight,
+        posShuriken,
+        posYShuriken,
+        shurikenSprite.charWidth,
+        shurikenSprite.charWidth
+      );
+    }
+  }
+  //returns which sprite from shuriken spritesheet to use
+  updateFrameShuriken() {
+    if (counter == 1) {
+      curFrameShuriken = (curFrameShuriken + 1) % 4;
+    }
+    //Calculating the x coordinate for spritesheet
+    return (srcXShuriken =
+      curFrameShuriken *
+      (shurikenSprite.spriteWidth / shurikenSprite.frameCount));
+  }
 }
 
 class menuBoxConstructor {
@@ -73,77 +143,103 @@ class menuBoxConstructor {
       (this.bottomY = bottomY);
   }
 }
+//enemytest
+const enemySprite = new SpriteAnimationConstructor(
+  3723,
+  468,
+  60,
+  75,
+  10,
+  runRight
+);
 
-const credits = new menuBoxConstructor(240, 367, 167, 194);
-const instructions = new menuBoxConstructor(210, 410, 117, 147);
+const creditsBox = new menuBoxConstructor(240, 367, 167, 194);
+const instructionsBox = new menuBoxConstructor(210, 410, 117, 147);
 const startGame = new menuBoxConstructor(223, 395, 64, 98);
-const shurikenSprite = new spriteAnimationConstructor(
+const backButton = new menuBoxConstructor(0, 50, 0, 50);
+const deathAnimationSprite = new SpriteAnimationConstructor(
+  4920,
+  508,
+  80,
+  80,
+  10,
+  deathAnimation
+);
+const shurikenSprite = new SpriteAnimationConstructor(
   1493,
   427,
   30,
   30,
   4,
-  shuriken.src
+  shuriken
 );
-const idleSprite = new spriteAnimationConstructor(
+const idleSprite = new SpriteAnimationConstructor(2420, 449, 40, 75, 10, idle);
+const idleLSprite = new SpriteAnimationConstructor(
   2420,
   449,
   40,
   75,
   10,
-  idle.src
+  idleL
 );
-const idleLSprite = new spriteAnimationConstructor(
-  2420,
-  449,
-  40,
-  75,
-  10,
-  idleL.src
-);
-const runLeftSprite = new spriteAnimationConstructor(
+const runLeftSprite = new SpriteAnimationConstructor(
   3723,
   468,
   60,
   75,
   10,
-  runleft.src
+  runLeft
 );
-const runRightSprite = new spriteAnimationConstructor(
+const runRightSprite = new SpriteAnimationConstructor(
   3723,
   468,
   60,
   75,
   10,
-  runright.src
+  runRight
 );
-const midJumpLeftSprite = new spriteAnimationConstructor(
+const midJumpLeftSprite = new SpriteAnimationConstructor(
   362,
   483,
   55,
   75,
   1,
-  jumpMidL.src
+  jumpMidL
 );
-const midJumpRightSprite = new spriteAnimationConstructor(
+const midJumpRightSprite = new SpriteAnimationConstructor(
   362,
   483,
   55,
   75,
   1,
-  jumpMidR.src
+  jumpMidR
 );
-const startMenuSprite = new spriteAnimationConstructor(
+const startMenuSprite = new SpriteAnimationConstructor(
   3723,
   468,
   40,
   40,
   10,
-  runright.src
+  runRight
 );
+
+//enemytest antall fiender man kan ha samtidig.
+const enemiesContainer = {
+  samurai: {
+    test1: false,
+    test2: false,
+    test3: false,
+    test4: false
+  },
+  ninja: {
+    test1: false,
+    test2: false,
+    test3: false,
+    test4: false
+  }
+};
 
 // Add click event listener to canvas element
-
 canvas.addEventListener("click", function(event) {
   // Button position and dimensions
   var muteX = 660;
@@ -170,11 +266,46 @@ canvas.addEventListener("click", function(event) {
     event.x - rect.left > startGame.leftX &&
     event.x - rect.left < startGame.rightX &&
     event.y - rect.top > startGame.topY &&
-    event.y - rect.top < startGame.bottomY
+    event.y - rect.top < startGame.bottomY &&
+    menu == true
   ) {
     posX = canvas.width / 2;
     posY = groundLevel;
     game = true;
+    menu = false;
+  }
+  if (
+    event.x - rect.left > instructionsBox.leftX &&
+    event.x - rect.left < instructionsBox.rightX &&
+    event.y - rect.top > instructionsBox.topY &&
+    event.y - rect.top < instructionsBox.bottomY &&
+    menu == true
+  ) {
+    menu = false;
+    instructions = true;
+  }
+  if (
+    event.x - rect.left > creditsBox.leftX &&
+    event.x - rect.left < creditsBox.rightX &&
+    event.y - rect.top > creditsBox.topY &&
+    event.y - rect.top < creditsBox.bottomY &&
+    menu == true
+  ) {
+    menu = false;
+    credits = true;
+  }
+  if (
+    event.x - rect.left > backButton.leftX &&
+    event.x - rect.left < backButton.rightX &&
+    event.y - rect.top > backButton.topY &&
+    event.y - rect.top < backButton.bottomY &&
+    menu == false
+  ) {
+    credits = false;
+    instructions = false;
+    game = false;
+    menu = true;
+    death = false;
   }
 });
 
@@ -193,19 +324,19 @@ canvas.addEventListener("mousemove", function(event) {
     menuCounter = 0;
   }
   if (
-    event.x - rect.left > instructions.leftX &&
-    event.x - rect.left < instructions.rightX &&
-    event.y - rect.top > instructions.topY &&
-    event.y - rect.top < instructions.bottomY &&
+    event.x - rect.left > instructionsBox.leftX &&
+    event.x - rect.left < instructionsBox.rightX &&
+    event.y - rect.top > instructionsBox.topY &&
+    event.y - rect.top < instructionsBox.bottomY &&
     game == false
   ) {
     menuCounter = 1;
   }
   if (
-    event.x - rect.left > credits.leftX &&
-    event.x - rect.left < credits.rightX &&
-    event.y - rect.top > credits.topY &&
-    event.y - rect.top < credits.bottomY &&
+    event.x - rect.left > creditsBox.leftX &&
+    event.x - rect.left < creditsBox.rightX &&
+    event.y - rect.top > creditsBox.topY &&
+    event.y - rect.top < creditsBox.bottomY &&
     game == false
   ) {
     menuCounter = 2;
@@ -214,89 +345,49 @@ canvas.addEventListener("mousemove", function(event) {
 
 //finner rett animasjon ift. bevegelse, og kjører changeAnimation.
 function findAnimation() {
-  if (
+  if (death == true) {
+    deathAnimationSprite.animateMainChar();
+  } else if (
     (!move.left && !move.right && dy == 0 && lastRight == true) ||
     (move.left && move.right && dy == 0 && lastRight == true)
   ) {
-    changeAnimation(idleSprite);
+    idleSprite.animateMainChar();
   } else if (
     (!move.left && !move.right && dy == 0 && lastRight == false) ||
     (move.left && move.right && dy == 0 && lastRight == false)
   ) {
-    changeAnimation(idleLSprite);
-  } else if (move.left && posY >= canvas.height - 110) {
-    changeAnimation(runLeftSprite);
-  } else if (move.right && posY >= canvas.height - 110) {
-    changeAnimation(runRightSprite);
+    idleLSprite.animateMainChar();
+  } else if (move.left && posY >= groundLevel) {
+    runLeftSprite.animateMainChar();
+  } else if (move.right && posY >= groundLevel) {
+    runRightSprite.animateMainChar();
   } else if (posY < groundLevel && lastRight == false) {
-    changeAnimation(midJumpLeftSprite);
+    midJumpLeftSprite.animateMainChar();
   } else {
-    changeAnimation(midJumpRightSprite);
+    midJumpRightSprite.animateMainChar();
   }
-}
-
-//endrer variablene som blir brukt for å endre animasjon, tilhørende animasjonstypen som blir kalt inn i funksjonen.
-function changeAnimation(sprite) {
-  animation.src = sprite.source;
-  spriteWidth = sprite.spriteWidth;
-  spriteHeight = sprite.spriteHeight;
-  frameCount = sprite.frameCount;
-  charWidth = sprite.charWidth;
-  charHeight = sprite.charHeight;
-}
-
-//animasjon shit
-function updateIndex() {
-  counter += 0.4;
-  if (counter == 1.6) {
-    counter = 0;
-  }
-  //Updating the frame index of sprite shit
-  curFrame = (curFrame + Math.floor(counter)) % frameCount;
-
-  //Calculating the x coordinate for spritesheet
-  srcX = (curFrame * spriteWidth) / frameCount;
-}
-
-function updateFrameShuriken() {
-  curFrameShuriken = (curFrameShuriken + Math.floor(counter)) % 4;
-  //Calculating the x coordinate for spritesheet
-  srcXShuriken =
-    curFrameShuriken * (shurikenSprite.spriteWidth / shurikenSprite.frameCount);
-}
-
-//tregner shuriken man kaster
-function drawShuriken() {
-  posShuriken += velocityShuriken;
-  ctx.drawImage(
-    shuriken,
-    srcXShuriken,
-    0,
-    shurikenSprite.spriteWidth / shurikenSprite.frameCount,
-    shurikenSprite.spriteHeight,
-    posShuriken,
-    posYShuriken,
-    shurikenSprite.charWidth,
-    shurikenSprite.charWidth
-  );
 }
 
 function drawSound() {
   ctx.drawImage(imageSound, 0, 0, 512, 512, 665, 0, 50, 50);
 }
-
-//tegner karakteren ift. bilde, sourcex og y, høyde bredde på bildetklippet, posisjon på kanvas, og karakter h/b
-function drawChar() {
+function drawBackButton() {
+  ctx.drawImage(imageBack, 0, 0, 512, 512, 9, 5, 40, 40);
+}
+//enemytest
+function drawEnemy() {
+  //enemytest
+  srcXenemies = (curFrame * enemySprite.spriteWidth) / enemySprite.frameCount;
   ctx.drawImage(
-    animation,
-    srcX,
+    runRight,
+    srcXenemies,
     0,
-    spriteWidth / frameCount,
+    enemySprite.spriteWidth / enemySprite.frameCount,
     spriteHeight,
-    posX,
-    posY,
-    charWidth,
-    charHeight
+    0,
+    groundLevel,
+    50,
+    50
   );
 }
 
@@ -307,7 +398,7 @@ function throwShuriken() {
   throwing = true;
   setTimeout(function() {
     throwing = false;
-  }, 2000);
+  }, 1170);
 }
 
 move = {
@@ -323,14 +414,13 @@ move = {
     switch (event.keyCode) {
       case 37: // venstre tast
         move.left = key_state;
-        lastRight = false;
         break;
       case 38: // opp tast
         //dobbelhopp, endre linje3 senere for bedre verdi
         let skyLevel = posY;
         if (upReleased == true) {
           if (doubleJump == false) {
-            dy += 18-(skyLevel/100); // 15
+            dy += 18 - skyLevel / 100; // 15
             doubleJump = true;
           }
         }
@@ -339,7 +429,6 @@ move = {
         break;
       case 39: // høyre tast
         move.right = key_state;
-        lastRight = true;
         break;
       case 68: // d tast
         move.d = key_state;
@@ -353,12 +442,15 @@ move = {
             posX = canvas.width / 2;
             posY = groundLevel;
             game = true;
+            menu = false;
           }
           if (menuCounter == 1) {
-            //instruksjons kode
+            instructions = true;
+            menu = false;
           }
           if (menuCounter == 2) {
-            //credits kode
+            credits = true;
+            menu = false;
           }
         }
     }
@@ -369,7 +461,7 @@ moveMenu = {
   down: false,
 
   keyListener: function(event) {
-    var key_state = event.type == "keydown" ? true : false;
+    var key_state = event.type == "keyup" ? true : false;
     switch (event.keyCode) {
       case 38:
         if (game == false) {
@@ -388,34 +480,44 @@ moveMenu = {
           menuCounter = 0;
         }
         break;
+      case 81:
+        if (death == false) {
+          curFrame = 0;
+          counter = 0;
+          death = true;
+        }
     }
   }
 };
 
 function moveChar() {
-  if (move.up) {
-    if (dy == 0) {
-      dy += 18;
-      doubleJump = false;
+  if (death == false) {
+    if (move.up) {
+      if (dy == 0) {
+        dy += 18;
+        doubleJump = false;
+        curFrame = 0;
+      }
+    }
+
+    if (move.left) {
+      dx -= 0.8;
+      lastRight = false;
+    }
+    if (move.right) {
+      dx += 0.8;
+      lastRight = true;
+    }
+
+    if (move.d && throwing == false) {
+      velocityShuriken = 10;
+      throwShuriken();
+    }
+    if (move.a && throwing == false) {
+      velocityShuriken = -10;
+      throwShuriken();
     }
   }
-
-  if (move.left) {
-    dx -= 0.8;
-  }
-  if (move.right) {
-    dx += 0.8;
-  }
-
-  if (move.d && throwing == false) {
-    velocityShuriken = 10;
-    throwShuriken();
-  }
-  if (move.a && throwing == false) {
-    velocityShuriken = -10;
-    throwShuriken();
-  }
-
   // Fysikk for mindre rigide bevegleser
   dy -= 0.6; // gravitasjon
   posX += dx; // akselerasjon x
@@ -440,32 +542,40 @@ function moveChar() {
 //"tegne" funksjonen
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // testmath = Math.floor(Math.random() * 400); //random enemy spawn algoritme enemytest, flere spawns vanskelighetsgrad
+  // if (testmath == 46 && enemiesContainer.test1 == false) {
+  //   enemiesContainer.test1 = true;
+  // }
   if (game == true) {
-    updateFrameShuriken();
     findAnimation();
-    updateIndex();
-    moveChar();
-    drawChar();
     drawSound();
-    if (throwing == true) {
-      drawShuriken();
-    }
-  } else if (game == false) {
-    menu();
+    drawBackButton();
+    shurikenSprite.drawShuriken();
+    moveChar();
   }
+  if (menu == true) {
+    menuDraw();
+    startMenuSprite.animateMainChar();
+  }
+  if (instructions == true) {
+    instructionsDraw();
+    drawBackButton();
+  }
+  if (credits == true) {
+    creditsDraw();
+    drawBackButton();
+  }
+
   requestAnimationFrame(draw);
 }
 
-function menu() {
+function menuDraw() {
   ctx.drawImage(menuBackground, 0, 0, 1453, 1024, 0, 0, 720, 480);
   ctx.font = "25px Arial";
+  ctx.fillStyle = "#000000";
   ctx.fillText("START GAME", 230, 90);
   ctx.fillText("INSTRUCTIONS", 218, 140);
   ctx.fillText("CREDITS", 252, 190);
-  moveChar();
-  changeAnimation(startMenuSprite);
-  updateIndex();
-  drawChar();
 
   if (menuCounter == 0) {
     posX = 390;
@@ -479,8 +589,21 @@ function menu() {
   }
 }
 
+function instructionsDraw() {
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, 720, 480);
+  ctx.rect(0, 0, 720, 480);
+  ctx.stroke();
+}
+
+function creditsDraw() {
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, 720, 480);
+  ctx.rect(0, 0, 720, 480);
+  ctx.stroke();
+}
+
 document.addEventListener("keydown", move.keyListener);
 document.addEventListener("keyup", move.keyListener);
 document.addEventListener("keyup", moveMenu.keyListener);
-
 draw();
