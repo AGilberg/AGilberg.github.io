2c783d77a86718ecd9028a4b2a33edb2993c2ffc
Havard <havardp_96@hotmail.no>
2018-11-13 17:41:14 +0100

    history istedenfor credits

--- kontakt-oss.js
+++ kontakt-oss.js
@@ -29,7 +29,7 @@ function validering() {
     document.getElementById("kommentar_sjekk").src = "false-bilde.png";
   }
 
-  if (email_1 >= 6) {
+  if (email_1 >= 6 && email.includes("@")) {
     document.getElementById("email_sjekk").src = "check-bilde.jpg";
   } else {
     document.getElementById("email_sjekk").src = "false-bilde.png";
@@ -42,13 +42,18 @@ function knappTrykk() {
   let enavn = document.getElementById("label2").value;
   let kommentar = document.getElementById("label5").value;
   let email = document.getElementById("label3").value;
-
   let for_1 = fnavn.length;
   let etter_1 = enavn.length;
   let komm_1 = kommentar.length;
   let email_1 = email.length;
 
-  if ((for_1 >= 2, etter_1 >= 2, komm_1 >= 5, email_1 >= 6)) {
+  if (
+    for_1 >= 2 &&
+    etter_1 >= 2 &&
+    komm_1 >= 5 &&
+    email_1 >= 6 &&
+    email.includes("@")
+  ) {
     alert("Takk for din hendvendelse");
   } else {
     alert(
