af82415f09495b0d5914e6824ead3aab8286e640
Havard <havardp_96@hotmail.no>
2018-11-13 11:29:35 +0100

    scroll fix

--- test2d.js
+++ test2d.js
@@ -862,3 +862,13 @@ function creditsDraw() {
 document.addEventListener("keydown", move.keyListener);
 document.addEventListener("keyup", move.keyListener);
 document.addEventListener("keyup", moveMenu.keyListener);
+document.addEventListener(
+  "keydown",
+  function(e) {
+    // space and arrow keys
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+    }
+  },
+  false
+);
